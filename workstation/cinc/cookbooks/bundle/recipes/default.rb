8cfd2dd99ccc5235f198e125b4e43d17cae79301
Stanislav Bebej <stanislav.bebej@gmail.com>
2026-08-06 22:30:43 +0200

    chore: make cinc more generic

--- workstation/cinc/cookbooks/bundle/recipes/default.rb
+++ workstation/cinc/cookbooks/bundle/recipes/default.rb
@@ -7,7 +7,7 @@
 # https://docs.fedoraproject.org/en-US/epel/getting-started/#_other_rhel_9_compatible_distributions
 execute "dnf --disableplugin=subscription-manager install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
 
-dnf_package %w(
+package %w(
     httpie
     jq
     nmap
